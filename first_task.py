92713453d7cf9443a422a95ed11c9e6812c62299
Zoznoba <maksimelyyan1@yandex.ru>
2022-06-16 22:51:03 +0300

    3 task complete

--- first_task.py
+++ first_task.py
@@ -1,2 +1,10 @@
-d = {k.lower(): v for _ in range(int(input("input n"))) for k, v in [input().split(": ")]}
-print(*(d.get(input().lower(), "не найдено") for _ in range(int(input("enter m")))), sep="\n")
+d = {}
+for _ in range(int(input())):
+    country, *cities = input().lower().split()
+    #cities = [city.lower() for city in cities]
+    d.update({country: cities})
+
+for _ in range(int(input())):
+    word = input().lower()
+    for key, value in d.items():
+        print(key.capitalize()) if word in value else False
